6ea63aa28978ad7de196ff22fc7ae230c510fd0f
RaulDiaz <rulhdiaz@gmail.com>
2023-05-27 13:33:02 -0600

    update

--- maps/src/Company.ts
+++ maps/src/Company.ts
@@ -1,14 +1,16 @@
 import modnarjs from "modnarjs";
 
-export class User {
-  name: string;
+export class Company {
+  companyName: string;
+  catchPhrase: string;
   location: {
     lat: number;
     lng: number;
-  };
+  }
 
   constructor() {
-    this.name = modnarjs.name.fName();
+    this.companyName = modnarjs.name.lName() + " " + 'ltd';
+    this.catchPhrase = modnarjs.name.fName() + " " + "phrase";
     this.location = {
       lat: modnarjs.number.float()*-1/10,
       lng: modnarjs.number.float()/10
