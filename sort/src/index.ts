7c97525825d3456974717a5ba42885868c7f5592
ruldiaz <rulhdiaz@gmail.com>
2023-05-28 20:26:48 -0600

    update

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,10 +1,29 @@
 class Sorter {
     
 
-    constructor( public collection: number[] ) {
+    constructor( public collection: number[] | string ) {
     }
 
-    sort():void {
+    sort(): void {
+        const { length } = this.collection;
+
+        if(this.collection instanceof Array){
+
+            for ( let i = 0 ; i < length ; i++ ) {
+                for ( let j = 0 ; j < length-i-1 ; j++ ){
+                    if(this.collection[j] > this.collection[j+1]){
+                        const leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j+1];
+                        this.collection[j+1] = leftHand;
+                    }
+                }
+            }
+        }
+
+        if( typeof this.collection === 'string' ) {
+            
+        }
+
 
     }
 }
@@ -13,4 +32,4 @@ const sorter = new Sorter([10, -3, -5, 0]);
 
 sorter.sort();
 
-console.log(sorter.collection);
+console.log(sorter.collection); 
