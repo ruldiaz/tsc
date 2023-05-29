c3395aa8aee539a30e8848dfd9f322bc2922a583
RaulDiaz <rulhdiaz@gmail.com>
2023-05-29 15:34:08 -0600

    update

---
+++ sort/src/CharactersCollection.ts
@@ -0,0 +1,13 @@
+export class CharactersCollection {
+  constructor(public data: string) {
+
+  }
+
+  get length(): number {
+    return this.data.length;
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase();
+  }
+}
