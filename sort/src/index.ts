0da288a1484668a69260c3df00c669d6c0dbf1af
ruldiaz <rulhdiaz@gmail.com>
2023-05-29 21:54:13 -0600

    update

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -3,18 +3,18 @@ import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
-const numbersCollection = new NumbersCollection([-10, 20, -5, 0]);
-numbersCollection.sort();
-// const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([50, -3, -5, 0]);
+// numbersCollection.sort();
+// // const sorter = new Sorter(numbersCollection);
 
-// sorter.sort();
+// // sorter.sort();
 
-console.log(numbersCollection.data); 
+// console.log(numbersCollection.data); 
 
 // const charactersCollection = new CharactersCollection('Xaayb');
-// const sorter = new Sorter(charactersCollection);
+// // const sorter = new Sorter(charactersCollection);
 
-// sorter.sort();
+// charactersCollection.sort();
 // console.log(charactersCollection.data);
 
 const linkedlist = new LinkedList();
