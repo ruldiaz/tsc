74ac36b6a3c03ed556b2e477d799cf61efb87b8e
ruldiaz <rulhdiaz@gmail.com>
2023-05-30 20:48:08 -0600

    update

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,15 +1,8 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
-const matches = fs_1.default.readFileSync('football.csv', {
-    encoding: 'utf-8'
-}).split('\n').map((row) => {
-    return row.split(',');
-});
-//console.log(matches);
+const CsvFileReader_1 = require("./CsvFileReader");
+const reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
 // enumeration
 var MatchResult;
 (function (MatchResult) {
@@ -19,7 +12,7 @@ var MatchResult;
 })(MatchResult || (MatchResult = {}));
 ;
 let manUnitedWins = 0;
-for (let match of matches) {
+for (let match of reader.data) {
     if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
     }
