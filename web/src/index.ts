ca74e61dec371de71f5ffe54673b9c168d0d3dca
RaulDiaz <rulhdiaz@gmail.com>
2023-06-01 15:36:24 -0600

    update

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,20 @@
-console.log('hi there!');
+// import { User } from "./models/User";
+import axios from 'axios';
+
+axios.get('http://localhost:3000/users/1');
+
+// const user = new User({name: 'myname', age: 20});
+
+// user.set({name: 'newname', age: 123});
+
+// user.on('change', ()=>{
+//   console.log('change # 1');
+// });
+
+// user.on('change', ()=>{
+//   console.log('change # 2');
+// });
+
+// user.trigger('change');
+
+// console.log(user);
