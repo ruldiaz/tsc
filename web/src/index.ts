e00d7d18becf69ab67147d401d30b8f36bd9b340
RaulDiaz <rulhdiaz@gmail.com>
2023-06-02 15:32:17 -0600

    update

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,7 +1,15 @@
 import { User } from "./models/User";
 
-const user = new User({id: 1, name: 'new record', age:0});
+const user = new User({name: 'new record', age:0});
 
 
-user.save()
+user.events.on('change', ()=>{
+  console.log('change');
+});
+
+user.events.on('change', ()=>{
+  console.log('change 2')
+})
+
+user.events.trigger('change');
 
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,12 +1,14 @@
-import axios, { AxiosResponse } from 'axios';
 
-interface UserProps {
+import { Eventing } from './Eventing';
+
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 };
 
 export class User {
+  public events: Eventing = new Eventing();
 
   constructor(private data: UserProps) {}
 
@@ -18,19 +20,5 @@ export class User {
     Object.assign(this.data, update);
   }
 
-  fetch(): void {
-    axios.get(`http://localhost:3000/users/${this.get('id')}`)
-    .then( (response: AxiosResponse ): void => {
-      this.set(response.data);
-    } )
-  }
 
-  save(): void {
-    const id = this.get('id');
-    if(id){
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      axios.post('http://localhost:3000/users', this.data);
-    }
-  }
 };
