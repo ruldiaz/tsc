620781251ff767e410efe64bff514f2f3114c8fd
RaulDiaz <rulhdiaz@gmail.com>
2023-06-05 13:45:31 -0600

    update

---
+++ features/decorators.ts
@@ -0,0 +1,35 @@
+class Boat {
+  @testDecorator
+  color: string = 'red';
+ 
+  @testDecorator
+  get formattedColor(): string {
+    return `This boats color is ${this.color}`;
+  }
+ 
+  @logError('Oops, boat was sunk in ocean')
+  pilot(): void {
+    throw new Error();
+    console.log('swish');
+  }
+}
+
+function testDecorator(target: any, key: string) {
+  console.log(key);
+}
+
+function logError(errorMessage: string){
+  return function(target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
+  
+    desc.value = function() {
+      try {
+        method();
+      } catch ( e ) {
+        console.log(errorMessage)
+      }
+    }
+  }
+} 
+
+
